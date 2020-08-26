e3b5c99b69d39a697515836c7f4bc78a05a8dd3b
Kojo Baffoe <63476577+kojobaffoe011@users.noreply.github.com>
2020-08-26 15:22:16 +0000

    edit fix

--- resources/views/blogs/create.blade.php
+++ resources/views/blogs/create.blade.php
@@ -1,5 +1,5 @@
 @extends('blogs.layout')
-  
+
 @section('content')
 <div class="row">
     <div class="col-lg-12 margin-tb">
@@ -11,21 +11,21 @@
         </div>
     </div>
 </div>
-   
+
 @if ($errors->any())
-    <div class="alert alert-danger">
-        <strong>Warning!</strong> Please check your input code<br><br>
-        <ul>
-            @foreach ($errors->all() as $error)
-                <li>{{ $error }}</li>
-            @endforeach
-        </ul>
-    </div>
+<div class="alert alert-danger">
+    <strong>Warning!</strong> Please check your input<br><br>
+    <ul>
+        @foreach ($errors->all() as $error)
+        <li>{{ $error }}</li>
+        @endforeach
+    </ul>
+</div>
 @endif
-   
+
 <form action="{{ route('blogs.store') }}" method="POST">
     @csrf
-     <div class="row">
+    <div class="row">
         <div class="col-xs-12 col-sm-12 col-md-12">
             <div class="form-group">
                 <strong>Title:</strong>
@@ -39,9 +39,9 @@
             </div>
         </div>
         <div class="col-xs-12 col-sm-12 col-md-12 text-center">
-                <button type="submit" class="btn btn-primary">Submit</button>
+            <button type="submit" class="btn btn-primary">Submit</button>
         </div>
     </div>
-   
+
 </form>
 @endsection
--- resources/views/blogs/layout.blade.php
+++ resources/views/blogs/layout.blade.php
@@ -4,15 +4,20 @@
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Layout</title>
+  <title>CRUD</title>
   <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 </head>
 
 <body>
-  <div style="color: crimson;" class="jumbotron text-center">
-    <h1>SIMPLE CRUD APP </h1>
-    <p>KOJO BAFFOE</p>
-  </div>
+  <header>
+    <div style="color: black;" class="jumbotron text-center">
+      <h1 style="font-weight: 300rem;">SIMPLE CRUD APP </h1>
+      <p style="font-size: 1.5rem;">KOJO BAFFOE</p>
+      <!-- <div class=" spinner-border" role="status">
+        <span class="sr-only"></span>
+      </div> -->
+    </div>
+  </header>
 
   <div class="container">
     @yield('content')
